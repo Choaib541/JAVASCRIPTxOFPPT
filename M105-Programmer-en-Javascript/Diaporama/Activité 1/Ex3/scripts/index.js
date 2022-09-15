0cd4813c07c1de62e934c890bba2192db14bc0fe
Choaib541 <mouhrachc@gmail.com>
2022-09-16 00:39:14 +0100

    fixin carousel

--- M105-Programmer-en-Javascript/Diaporama/Activité 1/Ex3/scripts/index.js
+++ M105-Programmer-en-Javascript/Diaporama/Activité 1/Ex3/scripts/index.js
@@ -1,16 +1,65 @@
-const IMAGES_NUMBER = 4;
-const images_container = document.querySelector('.images-container')
-let counter = 1;
+const images_container = document.querySelector(".images");
+const images = document.querySelectorAll("img");
+const stop = document.querySelector("#stop");
+const reset = document.querySelector("#reset");
 
-function translateX(value) {
-    images_container.style.cssText = `transform:translateY(-${value}px)`;
+
+class Carousel {
+    constructor(images, stopButton) {
+        this.id = null
+        this.images = images
+        this.cleared = true
+        this.stopButton = stopButton;
+    }
+
+    clear() {
+        this.cleared = true
+        this.stopButton.disabled = true
+    }
+
+    unClear() {
+        this.stopButton.disabled = false
+        this.cleared = false
+    }
+
+    reset_display_settings() {
+        this.images.forEach((e) => {
+            e.style.display = ""
+        })
+    }
+
+    start() {
+        this.reset_display_settings()
+        this.unClear()
+        let index = 0;
+        this.id = setInterval(() => {
+            if (index > 2) {
+                this.reset_display_settings()
+                index = 0
+            } else {
+                this.images[index].style.display = "none";
+                index += 1
+            }
+        }, 1500)
+    }
+
+    stop() {
+        if (!this.cleared) {
+            clearInterval(this.id)
+            this.clear()
+        } else {
+
+        }
+    }
+
+    reset() {
+        this.stop()
+        this.start()
+    }
 }
 
-function up() {
-    translateX(498 * counter)
-    counter += 1
-}
+const carousel = new Carousel(images, stop);
+carousel.start()
 
-// setInterval(() => {
-//     up()
-// },[400])
+stop.onclick = e => carousel.stop(e);
+reset.onclick = () => carousel.start();
